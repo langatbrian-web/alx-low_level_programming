2ff87d4b7585eb0d6367fba80ac0fd60922539b3
Brian Kibet <brian2lngt@gmail.com>
2022-12-09 14:03:16 -0800

    Updates

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: program to print alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch, CH;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		putchar(ch);
+
+	for (CH = 'A'; CH <= 'Z'; ++CH)
+		putchar(CH);
+
+	putchar ('\n');
+
+	return (0);
+}
