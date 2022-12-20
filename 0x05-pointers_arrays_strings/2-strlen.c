746e9c0031016020c2bf028bd8fea19a68a128c1
root <brianlangat@gmail.com>
2022-12-20 12:56:53 -0800

    Updates

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * int _strlen -a function that returns the length of a string.
+ * _strlen - a function that returns the length of a string.
  * @s: char input
  * Return: length of the input string
  */
