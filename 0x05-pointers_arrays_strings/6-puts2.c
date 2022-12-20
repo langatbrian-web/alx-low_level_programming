c79934821877bc8551d40ee3b7b9d828d69bfc0b
root <brianlangat@gmail.com>
2022-12-20 15:01:44 -0800

    Updates

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that should prit only one character out of two
+ * @str: input
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int n = 0;
+
+	for (; str[n] != '\0'; n++)
+	{
+		if ((n % 2) == 0)
+			_putchar(str[n]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
